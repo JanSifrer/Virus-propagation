871a34f071399e6aeafce26c27615d877b8a8d22
Jan Šifrer <jan.sifrer1@gmail.com>
2020-11-04 11:28:31 +0100

    Še malo popravil kodo.

--- generator_tock.py
+++ generator_tock.py
@@ -1,10 +1,29 @@
 import random
 import matplotlib.pyplot as plt
 
+### Najprej uvozimo potrebne pakete
+### Definiramo funckijo, ki nam nakljuèno doloèi koordinate toèk
+
 def koordinate_tocke():
     x = random.uniform(0, 1)
     y = random.uniform(0, 1)
-    return [x,y]
+    status = 0
+    return [x,y,status]
+
+### Definiramo funkcijo, ki nariše toèke
+
+def narisi_tocke(n,tocke,list):
+    okuzene_tocke = list
+    for j in range(0,n):
+        x = tocke["{}".format(j)][0]
+        y = tocke["{}".format(j)][1]
+        if j in okuzene_tocke:
+            plt.plot(x, y, 'o', color="red")
+        else:
+            plt.plot(x, y, 'o', color="black")
+    return plt.show()
+
+### Definiramo funckijo, ki nam zgenerira n toèk, in jih nariše
 
 def generiraj_n_tock(n):
     prva_tocka = koordinate_tocke()
@@ -14,17 +33,18 @@ def generiraj_n_tock(n):
         tocke.update({"{}".format(i):koordinate})
     
     okuzena_tocka = random.randrange(n)
-    for j in range(0,n):
-        x = tocke["{}".format(j)][0]
-        y = tocke["{}".format(j)][1]
-        if j == okuzena_tocka:
-            plt.plot(x, y, 'o', color="red")
-        else:
-            plt.plot(x, y, 'o', color="black")
-    return plt.show()
+    tocke["{}".format(okuzena_tocka)][2] = 1
+    okuzena_tocka = [okuzena_tocka]
+    narisi_tocke(n, tocke, okuzena_tocka)
+    return tocke
 
-a = koordinate_tocke()
+### TESTI
+#a = koordinate_tocke()
 b = generiraj_n_tock(200)
-b
 
-
+# pogledamo katera toèka je okužena
+xcx = 0
+for i in range(0,len(b)):
+    tocka = b["{}".format(i)][2]
+    if tocka == 1:
+        print(i, b["{}".format(i)])
