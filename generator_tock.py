686a0ea1375a08978877616d51cf1d0f0ba71aa8
Jan Šifrer <jan.sifrer1@gmail.com>
2020-12-07 12:26:54 +0100

    Dodal komentarje h kodi.

--- generator_tock.py
+++ generator_tock.py
@@ -5,8 +5,10 @@ import csv
 import os.path
 import io, urllib, base64
 
-### Najprej uvozimo potrebne pakete
-### Definiramo funckijo, ki nam nakljucno doloci koordinate tock
+### Najprej uvozimo potrebne pakete.
+### Definiramo funckijo, ki nam nakljucno določi koordinate točk, 
+### ter hkrati se zabeleži, da točka ni ne aktivno okužena, in da še ni 
+### prebolela virusa.
 
 def koordinate_tocke():
     x = random.uniform(0, 1)
@@ -15,7 +17,7 @@ def koordinate_tocke():
     ze_prebolela = 0
     return [x,y,koliko_dni_se_kuzna, ze_prebolela]
 
-### Definiramo funckijo, ki nam zgenerira n tock
+### Definiramo funckijo, ki nam zgenerira n tock.
 
 def generiraj_n_tock(n):
     prva_tocka = koordinate_tocke()
@@ -26,8 +28,9 @@ def generiraj_n_tock(n):
     return tocke
 
 ### Definiramo funkcijo, ki nam ustvari slovar sosedov, 
-### pogledamo najprej ali je kandidat sosed od neke dolocene tocke,
-### ce je, dodamo to tocko v seznam sosedov od kandidata.
+### pogledamo najprej ali je kandidat sosed od neke določene okužene točke,
+### če je, dodamo to točko v seznam sosedov od kandidata.
+
 def ali_je_sosed(kandidat, tocka, tocke, radij, dict_sosedov):
     x_tocke, y_tocke = tocke[tocka][0:2]
     x_kandidat, y_kandidat = tocke[kandidat][0:2]
@@ -38,45 +41,59 @@ def ali_je_sosed(kandidat, tocka, tocke, radij, dict_sosedov):
             dict_sosedov[kandidat] += [tocka]
     return dict_sosedov
 
-### Dolocimo funckijo, ki nam narise tocke
+### Definiramo funckijo, ki nam shrani slike, da jih potem lahko narišemo
+### v datoteki zacetna.html.
 def za_risanje_tock(n,tocke, dict_sosedov, korak_slike, ze_poznane_slike):
+    ### Najprej pobrišemo vse slike, ki jih je do sedaj že narisal (to prav pride, 
+    ### če program večkrat zaženemo).
     plt.clf()
     for k, (x, y, koliko_dni_se_kuzna, ze_prebolela) in tocke.items():
+        ### Če je točka že prebolela virus jo pobarvamo z modro.
         if ze_prebolela == 1:
             plt.plot(x, y, 'o', color="blue")
         else:
+            ### Okužene pobarvamo z rdečo.
             if koliko_dni_se_kuzna > 0:
                 plt.plot(x, y, 'o', color="red")
             else:
+                ### Sosede pobarvamo zeleno.
                 if len(dict_sosedov[k]) > 0:
                     plt.plot(x, y, 'o', color="green")
+                ### Če točka ni ne prebolela okužbe, ni ne aktivno okuzena, 
+                ### in ni ne sosed kake okužene, jo pobarvamo s črno.
                 else:
                     plt.plot(x, y, 'o', color="black")
+    ### Dodamo še naslov slike.
     plt.suptitle("slika na {} koraku".format(korak_slike))
     korak_slike = io.BytesIO()
     plt.savefig(korak_slike, format="png")
     korak_slike.seek(0)
     string = base64.b64encode(korak_slike.read())
     uri = 'data:image/png;base64,' + urllib.parse.quote(string)
+    ### Slovar poznanih slik posodobimo z novo sliko.
     ze_poznane_slike.update({korak_slike:uri})
     return ze_poznane_slike
 
 def narisi_m_okuzenih_tock(n, m, tocke, T, radij):
-    ### Najprej dediniram slovar sosedov, in zapisem, da nobena tocka ni sosed od kere okuzene
+    ### Najprej definiramo slovar sosedov, in zapišem, da nobena točka ni sosed od kere okužene.
     dict_sosedov = {}
     for i in range(0,n):
         dict_sosedov.update({i:[]})
     for i in range(0,m):
-        ### Izberem neko nakljucno okuzeno tocko, in nastavim da bo kuzna se T dni 
+        ### Izberemo neko naključno okuženo točko, in nastavimo da bo kužna še T dni.
         okuzena_tocka = random.randrange(n)
         tocke[okuzena_tocka][2] = T
         for j in range(0,n):
-            ### Za vse tocke preverim, ali so sosedi okuzene tocke
+            ### Za vse točke preverimo, ali so sosedi okužene točke.
             dict_sosedov = ali_je_sosed(j, okuzena_tocka, tocke, radij, dict_sosedov)
-    ### Narisem prvo stanje
+    ### Narišemo oziroma shranimo prvo stanje - ena točka bo rdeča, nekaj jih bo zelenih, ostale bodo črne.
     ze_poznane_slike = za_risanje_tock(n, tocke, dict_sosedov, 1, {})
     return tocke, dict_sosedov, ze_poznane_slike
 
+### Sedaj pa definirajmo še glavno funckijo, ki bo združevala vse prejšnje, in nam bo omogočala zagon same simulacije.
+### Kot vhodne podatke moramo vnesti na koliko točkah želimo simulacijo, koliko želimo imeti na začetku okuženih,
+### kakšna je verjetnost prenosa okužbe, koliko dni je točka lahko kužna, maksimalno število korakov simulacije,
+### na kakšni razdalji so sosedi od okužene točke, na koliko korakov želimo videti stanje, in ali sploh želimo videti stanje oz. sliko.
 def okuzi_sosede(n, koliko_zacetnih_okuzenih, verjetnost, T, max_st_ponovitev, radij, koraki_do_slike, ali_risem_slike):
     tocke = generiraj_n_tock(n)
     tocke, dict_sosedov, ze_poznane_slike = narisi_m_okuzenih_tock(n, koliko_zacetnih_okuzenih, tocke, T, radij)
@@ -84,59 +101,66 @@ def okuzi_sosede(n, koliko_zacetnih_okuzenih, verjetnost, T, max_st_ponovitev, r
     while max_st_ponovitev > 0:
         max_st_ponovitev -= 1
         koraki += 1
+        ### Gremo po vseh točkah iz slovarja in preberemo vse njihove podatke.
         for k, (x, y, koliko_dni_se_kuzna, ze_prebolela) in tocke.items():
+            ### Če je točka že prebolela virus, ne naredimo ničesar.
             if ze_prebolela < 1:
+                ### Če je točka že okužena, ne naredimo ničesar.
                 if koliko_dni_se_kuzna < 1:
-                    ### Najpprej preverimo, ce je sosed od kaksne okuzene tocke, in ce je, od koliko tock je sosed
+                    ### Najprej preverimo, če je soseda od kaksne okužene točke, in če je, od koliko točk je soseda.
                     kolikokart_sosed = len(dict_sosedov[k])
                     if kolikokart_sosed > 0:
-                        ### Toliko kolikor ima okuzenih sosedov, tolikokrat je lahko z doloceno verjetnostjo okuzen
+                        ### Toliko kolikor ima okuženih sosedov, tolikokrat je lahko z določeno verjetnostjo okužena.
                         ali_se_okuzi = np.random.binomial(size=kolikokart_sosed, n=1, p=verjetnost)
                         ali_se_okuzi = max(ali_se_okuzi)
                         if ali_se_okuzi > 0:
-                            ### Ce se okuzi, nastavimo cas kuznosti na T+1 (T je cas kuznosti, +1 pa mi v naslednji dveh 
-                            ### for zankah omogoca, da popravim sosede od nje...)
+                            ### Če se okuži, nastavimo čas kužnosti na T+1 (T je cas kužnosti, +1 pa nam v naslednji dveh 
+                            ### for zankah omogoča, da popravim sosede od nje...).
                             tocke[k][2] = T+1
+        ### Definirajmo še dve spremenljivki, ki nam bosta šteli koliko je aktivno okuženih, in koliko je virus že prebolelo.
         stevilo_trenutno_bolnih = 0
         preboleli = 0
         for k, (x, y, koliko_dni_se_kuzna, ze_prebolela) in tocke.items():
+            ### Z naslednjima dvema if stavkama samo poskrbimo, da obravnavamo samo točke, ki še niso prebolele virusa, in ki so okužene.
             if ze_prebolela < 1:
-                ### Za vsako tocko pogledamo koliko dni bo se kuzna, ce ni kuzna, vrne 0
                 if koliko_dni_se_kuzna > 0:
                     for j in range(0,n):
                         if koliko_dni_se_kuzna > 1:
-                            ### Ce bo tocka okuzena se vec kakor 1 dan, potem za vse druge tocke pogledam,
-                            ### ce je soseda od okuzene tocke
+                            ### Če bo točka okužena še več kakor 1 dan, potem za vse druge točke pogledam,
+                            ### če je katera soseda od te okužene točke.
                             dict_sosedov = ali_je_sosed(j, k, tocke, radij, dict_sosedov)
                         if koliko_dni_se_kuzna == 1:
-                            ### Če je to zadnji dan, ko je oseba se kuzna, popravimo seznam sosedov,
-                            ### kajti ta tocka (j) ne bo vec soseda od okuzene tocke (k)
+                            ### Če je to zadnji dan, ko je točka še kužna, popravimo seznam sosedov,
+                            ### kajti ta tocka (j) ne bo vec soseda od okužene točke (k).
                             try:
                                 dict_sosedov[j].remove(k)
                             except:
                                 pass
+                    ### Če je točka kužna samo še en dan, še nastavimo, da je prebolela virus.
                     if koliko_dni_se_kuzna == 1:
                         tocke[k][3] = 1
-
-                    ### Na koncu se popravimo, koliko dni bo se kuzna.
+                    ### Na koncu še popravimo, koliko dni bo še kužna.
                     tocke[k][2] -= 1
-                ### stevilo_trenutno_bolnih nam šteje število okuženih točk
+                ### stevilo_trenutno_bolnih nam šteje število okuženih točk.
                 if koliko_dni_se_kuzna > 1:
                     stevilo_trenutno_bolnih +=1
-                ### Preboleli je število prebolelih
+                ### Preboleli nam šteje koliko točk je virus prebolelo.
                 if koliko_dni_se_kuzna == 1:
                     preboleli += 1
             else:
                 preboleli += 1
-        ### Narisemo stanje, samo če je ali_risem_slike = True
+        ### Narišemo oziroma si shranimo stanje ampak samo če je ali_risem_slike = True.
         if ali_risem_slike:
+            ### Za sledenje kje je program, še nastavimo da na določenih korakih izpisuje razmere - koliko je trenutno okuženih in koliko
+            ### točk je virus prebolelo.
             if koraki % koraki_do_slike == 0:
                 print("stevilo okuzenih pri {0} koraku je: {1}, zdravih pa {2}".format(koraki, stevilo_trenutno_bolnih, preboleli))
                 ze_poznane_slike = za_risanje_tock(n, tocke, dict_sosedov, koraki, ze_poznane_slike)
 
             if koraki % 10 == 0:
                 print("stevilo okuzenih pri {0} koraku je: {1}, zdravih pa {2}".format(koraki, stevilo_trenutno_bolnih, preboleli))
-        
+
+            ### Še robna primera, če so vse točke okužene, ali pa če ni nobena več, se program ustavi.
             if (stevilo_trenutno_bolnih)/n == 1:
                 print("stevilo okuzenih pri {0} koraku je: {1}".format(koraki, stevilo_trenutno_bolnih))
                 print("Delez okuzenih je: {}".format(stevilo_trenutno_bolnih/n))
@@ -147,47 +171,35 @@ def okuzi_sosede(n, koliko_zacetnih_okuzenih, verjetnost, T, max_st_ponovitev, r
                 print("stevilo okuzenih pri {0} koraku je: {1}, zdravih pa {2}".format(koraki, stevilo_trenutno_bolnih, preboleli))
                 ze_poznane_slike = za_risanje_tock(n, tocke, dict_sosedov, koraki, ze_poznane_slike)
                 return(ze_poznane_slike)
-       ### Če ne želimo da riše slike, tukaj nadaljuje:
+       ### Če ne želimo da riše slike, nadaljujemo samo z robnima primeroma (uporabno če animacijo velikokrat poženemo, 
+       ### tako kot npr. v funckiji generiraj()).
         if (stevilo_trenutno_bolnih)/n == 1:
             return (stevilo_trenutno_bolnih, preboleli, koraki)
         if (stevilo_trenutno_bolnih)/n == 0:
             return (stevilo_trenutno_bolnih, preboleli, koraki)
     return (stevilo_trenutno_bolnih, preboleli, koraki)
             
+### Definirajmo še funckijo, ki nam bo zgenerirala podatke.
 def generiraj():
+    ### Začnemo z verjetnostjo okužbe 0.02 in radijam 0.02.
     verjetnost = 0.02
     radij = 0.02
     datoteka = "generirani_podatki2.csv"
+    ### V .csv datoteko zapišemo glavo.
     with open(datoteka, 'a', newline='') as data:
         writer = csv.DictWriter(data, fieldnames =["Verjetnost", "Radij", "Okuženi", "Preboleli", "Koraki"])
         writer.writeheader()
-    for j in range(0,4):
+    ### Sprehodimo se čez vse kombinacije radijev 0.02, 0.04 in 0.06 ter verjetnosti 0.02, 0.04, 0.06, 0.08 in 0.1 in
+    ### za vsako kombinacijo 100 krat poženemo simulacijo.
+    for j in range(0,3):
         radij = 0.02 + j*0.02
         for h in range(0, 5):
             verjetnost = 0.02 + h*0.02
             for i in range(0,100):
                 steviloo, preboleli, koraki = okuzi_sosede(2000, 1, verjetnost, 14, 1000, radij,1, False)
+                ### Na vsakem koraku še izpišemo podatke, da je čakanje malo bolj prijetno :).
                 print(i, radij, verjetnost, steviloo, preboleli, koraki)
+                ### Nato pa podatke še zapišemo v .csv datoteko.
                 with open(datoteka, 'a', newline='') as data:
                     writer = csv.DictWriter(data, fieldnames =["Verjetnost", "Radij", "Okuženi", "Preboleli", "Koraki"])
                     writer.writerow({"Verjetnost":"{0}".format(verjetnost), "Radij":"{0}".format(radij), "Okuženi": "{0}".format(steviloo), "Preboleli": "{0}".format(preboleli), "Koraki": "{0}".format(koraki)})
-
-
-
-
-
-
-
-#tocke = generiraj_n_tock(2000)
-#### TESTI
-#a,b,c = narisi_m_okuzenih_tock(2000, 1, tocke, 10, 0.2)
-#for i,v in c.items():
-#    print(v)
-#b = narisi_m_okuzenih_tock(200, 2, tocke, 10, 0.2)
-#
-#b = okuzi_sosede(2000, 1, 0.01, 14, 1000, 0.05, 2)
-#countt = 0
-#for i in b:
-#    if b["{}".format(i)][3] == 0:
-#        countt +=1
-#        #print(i)
